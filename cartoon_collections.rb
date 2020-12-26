fe548c67100449baca63633a047369b97ce17622
Harrf Akbar <harrf_akbar@yahoo.com>
2020-12-26 20:07:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,6 +5,7 @@ def greet_characters(array)
   # Print a custom greeting for each element
   array.each do | character |
     puts " Hello #{ character}"
+  end
 end
 
 def list_dwarves(array)
@@ -12,4 +13,5 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index { | element, index | array[element] = index}
+
 end
